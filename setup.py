b6d35f837e544d7d2f1c8ef6e29b41ecc1993283
akkiyolo <akshatshukla069@gmail.com>
2026-08-08 18:19:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+  name="us-visa",
+  version="0.0.0",
+  author="Akki",
+  author_email="akshatshukla069@gmail.com",
+  packages=find_packages()
+)
