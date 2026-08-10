7add558bab2e110f3270d0572fb8d47e1e05c72a
akkiyolo <akshatshukla069@gmail.com>
2026-08-10 09:44:49 +0530

    data ingestion constants updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -11,3 +11,12 @@ PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR = "artifact"
 
 MODEL_FIT_NAME = "model.pkl"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
